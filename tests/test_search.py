210d2ce025b75e1389130fb6414545a81b91fb8a
vasylhladyshev404 <vasyl@nextgear.nl>
2025-10-29 14:19:22 +0100

    1

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+from taxi.models import Car, Manufacturer
+
 
 class DriverSearchTest(TestCase):
     def setUp(self):
@@ -30,6 +32,76 @@ class DriverSearchTest(TestCase):
     def test_search_query_no_match(self):
         response = self.client.get(
             reverse("taxi:driver-list"),
+            {"query": "zzz"}
+        )
+        self.assertNotContains(response, "alex")
+        self.assertNotContains(response, "maria")
+
+
+class CarSearchTest(TestCase):
+    def setUp(self):
+        self.manufacturer = Manufacturer.objects.create(
+            name="Test Manufacturer",
+            country="Testland"
+        )
+        self.car1 = Car.objects.create(
+            model="BMW",
+            manufacturer=self.manufacturer
+        )
+        self.car2 = Car.objects.create(
+            model="mers",
+            manufacturer=self.manufacturer
+        )
+
+    def test_search_no_query_returns_all(self):
+        response = self.client.get(reverse("taxi:car-list"))
+        self.assertContains(response, "bmw")
+        self.assertContains(response, "mers")
+
+    def test_search_query_matches_one(self):
+        response = self.client.get(
+            reverse("taxi:car-list"),
+            {"query": "bmw"}
+        )
+        self.assertContains(response, "bmw")
+        self.assertNotContains(response, "mers")
+
+    def test_search_query_no_match(self):
+        response = self.client.get(
+            reverse("taxi:car-list"),
+            {"query": "zzz"}
+        )
+        self.assertNotContains(response, "bmw")
+        self.assertNotContains(response, "mers")
+
+
+class ManufacturerSearchTest(TestCase):
+    def setUp(self):
+        self.manufacturer1 = Manufacturer.objects.create(
+            name="alex",
+            country="Testland"
+        )
+        self.manufacturer2 = Manufacturer.objects.create(
+            name="maria",
+            country="Testland2"
+        )
+
+    def test_search_no_query_returns_all(self):
+        response = self.client.get(reverse("taxi:manufacturer-list"))
+        self.assertContains(response, "alex")
+        self.assertContains(response, "maria")
+
+    def test_search_query_matches_one(self):
+        response = self.client.get(
+            reverse("taxi:manufacturer-list"),
+            {"query": "alex"}
+        )
+        self.assertContains(response, "alex")
+        self.assertNotContains(response, "maria")
+
+    def test_search_query_no_match(self):
+        response = self.client.get(
+            reverse("taxi:manufacturer-list"),
             {"query": "zzz"}
         )
         self.assertNotContains(response, "alex")
